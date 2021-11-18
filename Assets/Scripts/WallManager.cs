ee6b9e3b631874a078b3ba5694d7bf97dc82554c
Dhaivat Patel <dhaivat.dhp@gmail.com>
2021-11-18 18:11:34 -0500

    Updates UI

--- Assets/Scripts/WallManager.cs
+++ Assets/Scripts/WallManager.cs
@@ -4,79 +4,43 @@ using UnityEngine;
 
 public class WallManager : MonoBehaviour
 {
-    public float wallProb = 0.3f;
-    public float wallSpawnRadius = 20;
-    public GameObject wallPrefab;
-
+    public static WallManager inst;
+    public GameObject prefabW;
     private List<Agent> walls = new List<Agent>();
-    private GameObject wallParent;
-    private static HashSet<GameObject> wallObjs = new HashSet<GameObject>();
-
-    public static WallManager instance;
+    public float wallProb = 0.3f;
+    public float wsp = 20;
+    private GameObject wp;
+    private static HashSet<GameObject> allWalls = new HashSet<GameObject>();
 
 
     void Start()
     {
-        instance = this;
+        inst = this;
         Random.InitState(0);
 
-        wallParent = GameObject.Find("Walls");
-        for (int i = -Mathf.RoundToInt(wallSpawnRadius / 2); i < wallSpawnRadius / 2; i++)
-            for (int j = -Mathf.RoundToInt(wallSpawnRadius / 2); j < wallSpawnRadius / 2; j++)
+        wp = GameObject.Find("Walls");
+        for (int i = -Mathf.RoundToInt(wsp / 2); i < wsp / 2; i++)
+            for (int j = -Mathf.RoundToInt(wsp / 2); j < wsp / 2; j++)
             {
                 if (Random.value < wallProb)
                 {
                     GameObject wall = null;
-                    wall = Instantiate(wallPrefab, new Vector3(i + 0.5f, 0.5f, j + 0.5f), Quaternion.identity);
+                    wall = Instantiate(prefabW, new Vector3(i + 0.5f, 0.5f, j + 0.5f), Quaternion.identity);
                     wall.name = "Wall " + i;
-                    wall.transform.parent = wallParent.transform;
+                    wall.transform.parent = wp.transform;
                     var wallScript = wall.GetComponent<Agent>();
 
                     walls.Add(wallScript);
-                    wallObjs.Add(wall);
+                    allWalls.Add(wall);
                 }
             }
-        /*for (int i = -Mathf.RoundToInt(wallSpawnRadius / 2); i < wallSpawnRadius / 2; i++)
-        {
-            foreach (var j in new[] { -Mathf.RoundToInt(wallSpawnRadius / 2) - 1, wallSpawnRadius / 2 })
-            {
-                GameObject wall = null;
-                wall = Instantiate(wallPrefab, new Vector3(i + 0.5f, 0.5f, j + 0.5f), Quaternion.identity);
-                wall.name = "Wall " + i;
-                wall.transform.parent = wallParent.transform;
-                var wallScript = wall.GetComponent<Agent>();
-
-                walls.Add(wallScript);
-                wallObjs.Add(wall);
-            }
-        }
-        for (int j = -Mathf.RoundToInt(wallSpawnRadius / 2); j < wallSpawnRadius / 2; j++)
-        {
-            foreach (var i in new[] { -Mathf.RoundToInt(wallSpawnRadius / 2) - 1, wallSpawnRadius / 2 })
-            {
-                GameObject wall = null;
-                wall = Instantiate(wallPrefab, new Vector3(i + 0.5f, 0.5f, j + 0.5f), Quaternion.identity);
-                wall.name = "Wall " + i;
-                wall.transform.parent = wallParent.transform;
-                var wallScript = wall.GetComponent<Agent>();
-
-                walls.Add(wallScript);
-                wallObjs.Add(wall);
-            }
-        }*/
 
     }
-
-    void Update()
-    {
-        
-    }
-
     #region Public Functions
 
     public static bool IsWall(GameObject obj)
     {
-        return wallObjs.Contains(obj);
+        return allWalls.Contains(obj);
     }
 
     #endregion
